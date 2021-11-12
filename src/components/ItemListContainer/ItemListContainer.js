a2c73152d98aa9eee9e0864c3b281ea0325807ba
Yunnia21 <Dame.Viviana.du.lac@gmail.com>
2021-11-12 19:01:27 -0300

    Comienzo

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,8 @@
+export const ItemListContainer = () => {
+
+    return (
+        <div>
+            <h2>Holi Roler@!</h2>            
+        </div>
+    )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+export const NavBar = () => {
+
+    return (
+        <div>
+            <h2>Powered by the Apocalypse</h2>
+            <nav>
+                <ul>
+                    <li>Apocalypse World</li>
+                    <li>The Warren</li>
+                    <li>Dungeon World</li>
+                    <li>Worlds in Peril</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
