add683ba8898299b8a05b3eaa023cd874a548e97
Yunnia21 <Dame.Viviana.du.lac@gmail.com>
2021-11-16 00:50:17 -0300

    botón quitar

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,19 +1,27 @@
 import {React, useState} from 'react'
 
 export function ItemCount({inicial, stock, elemento}) {
+           
     const [count, setCount] = useState(inicial)
+    const quitar = (cantidad) => {
+        setCount(count - cantidad) 
+       
+    }
+    const onRemove=(quitar)=> { 
+        console.log("Quitaste de la Librería")
+    }
+
+    
     const agregar = (cantidad) => {
         setCount(count + cantidad)
-    }
-    const quitar = (cantidad) => {
-        setCount(count - cantidad)   }
-    
+    }    
     const onAdd=(cantidad)=> {
         console.log("Agregaste a la Librería");
     }
+    
     return(
         <div>
-        <button className="btn_cart" onClick={() => onAdd(quitar) }
+        <button className="btn_cart" onClick={() => onRemove(quitar) }
         >Quitar
         </button>
         <button className="btn_cart" onClick={() => onAdd(count)}>Agregar a la librería
